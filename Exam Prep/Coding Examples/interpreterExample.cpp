8f32b24d1177ca55d34965ccae6eb0912748c881
Matthew Schoeman <61004307+MT-Cobbler@users.noreply.github.com>
2021-12-14 10:21:14 +0200

    okay

--- Exam Prep/Coding Examples/interpreterExample.cpp
+++ Exam Prep/Coding Examples/interpreterExample.cpp
@@ -4,127 +4,148 @@
 using namespace std;
 class Context;
 class Variables;
-class Expression{
-    public:
-        Expression(void){}
-        virtual ~Expression(void){}
-        virtual bool evaluate(Context& c) = 0;
+class Expression
+{
+public:
+    Expression(void) {}
+    virtual ~Expression(void) {}
+    virtual bool evaluate(Context &c) = 0;
 };
 
 // Non terminal Expressions
-class And : public Expression{
-    public:
-        And(Expression* l, Expression* r) : op1(l), op2(r){}
-        virtual ~And(){}
-        virtual bool evaluate(Context& c){
-            bool op1Evalutaion = op1->evaluate(c);
-            bool op2Evalutaion = op2->evaluate(c);
-            return op1Evalutaion && op2Evalutaion;
-        }
-    private:
-        Expression* op1;
-        Expression* op2;
+class And : public Expression
+{
+public:
+    And(Expression *l, Expression *r) : op1(l), op2(r) {}
+    virtual ~And() {}
+    virtual bool evaluate(Context &c)
+    {
+        bool op1Evalutaion = op1->evaluate(c);
+        bool op2Evalutaion = op2->evaluate(c);
+        return op1Evalutaion && op2Evalutaion;
+    }
+
+private:
+    Expression *op1;
+    Expression *op2;
 };
 
-class Or : public Expression{
-    public:
-        Or(Expression* l, Expression* r) : op1(l), op2(r){
-            cout << "Expressions added" << endl;
-        }
-        virtual ~Or(){}
-        virtual bool evaluate(Context& c){
-            bool op1Evalutaion = op1->evaluate(c);
-            bool op2Evalutaion = op2->evaluate(c);
-            return op1Evalutaion || op2Evalutaion;
-        }
-    private:
-        Expression* op1;
-        Expression* op2;
+class Or : public Expression
+{
+public:
+    Or(Expression *l, Expression *r) : op1(l), op2(r)
+    {
+        cout << "Expressions added" << endl;
+    }
+    virtual ~Or() {}
+    virtual bool evaluate(Context &c)
+    {
+        bool op1Evalutaion = op1->evaluate(c);
+        bool op2Evalutaion = op2->evaluate(c);
+        return op1Evalutaion || op2Evalutaion;
+    }
+
+private:
+    Expression *op1;
+    Expression *op2;
 };
 
-class Not : public Expression{
-    public:
-        Not(Expression* l) : op1(l){}
-        virtual ~Not(){}
-        virtual bool evaluate(Context& c){
-            bool op1Evalutaion = op1->evaluate(c);
-            return !op1Evalutaion;
-        }
-    private:
-        Expression* op1;
+class Not : public Expression
+{
+public:
+    Not(Expression *l) : op1(l) {}
+    virtual ~Not() {}
+    virtual bool evaluate(Context &c)
+    {
+        bool op1Evalutaion = op1->evaluate(c);
+        return !op1Evalutaion;
+    }
+
+private:
+    Expression *op1;
 };
 
 // Terminal Expressions
-class Variables : public Expression{
-    public:
-        Variables(const string n) : name(n){cout << "Variable " << n << " added!" << endl;}
-        virtual ~Variables(){}
-        virtual bool evaluate(Context& c){
-            c.lookup(this->getName());
-            return c.lookup(name);
-        }
-        string getName(){return this->name;}
-    private:
-        string name;
+class Variables : public Expression
+{
+public:
+    Variables(const string n) : name(n) { cout << "Variable " << n << " added!" << endl; }
+    virtual ~Variables() {}
+    virtual bool evaluate(Context &c)
+    {
+        c.lookup(this->getName());
+        return c.lookup(name);
+    }
+    string getName() { return this->name; }
+
+private:
+    string name;
 };
 
+class Constant : public Expression
+{
+public:
+    Constant(bool b) : op1(b) {}
+    virtual ~Constant() {}
 
-class Constant : public Expression{
-    public:
-        Constant(bool b) : op1(b){}
-        virtual ~Constant(){}
+    virtual bool evaluate(Context &c)
+    {
+        return op1;
+    }
 
-        virtual bool evaluate(Context& c){
-            return op1;
-        }
-    private:
-        bool op1;
+private:
+    bool op1;
 };
 
-class Context{
-   public:
-        Context(){}
-        ~Context(){}
-        bool lookup(string l){
-            if(nameValue.find(l) != nameValue.end()){
-                return nameValue.find(l)->second;
-            }
-            return false;
+class Context
+{
+public:
+    Context() {}
+    ~Context() {}
+    bool lookup(string l)
+    {
+        if (nameValue.find(l) != nameValue.end())
+        {
+            return nameValue.find(l)->second;
         }
-        void assign(Variables* e, bool s){
-            nameValue[e->getName()] = s;
-        }
-   private: 
-        map<string, bool> nameValue;
+        return false;
+    }
+    void assign(Variables *e, bool s)
+    {
+        nameValue[e->getName()] = s;
+    }
+
+private:
+    map<string, bool> nameValue;
 };
 int main()
 {
-    Expression* expression;
+    Expression *expression;
     Context context;
 
-    Variables* x = new Variables("X");
-    Variables* y = new Variables("Y");
-    Variables* z = new Variables("zValue");
-    
-    expression = new Or(
-        new And (new Constant(true), x),
-        new And (y, new Not(z)));
+    Variables *x = new Variables("X");
+    Variables *y = new Variables("Y");
+    Variables *z = new Variables("zValue");
 
-    context.assign(x,0);
-    context.assign(y,0);
-    context.assign(z,1);    
-	
+    expression = new Or(
+        new And(new Constant(true), x),
+        new And(y, new Not(z)));
+
+    context.assign(x, 0);
+    context.assign(y, 0);
+    context.assign(z, 1);
+
     bool result = expression->evaluate(context);
 
-    cout << "Overall result is " << (result == 0 ? "false": "true") << endl;
+    cout << "Overall result is " << (result == 0 ? "false" : "true") << endl;
 
-    context.assign(x,true);
-    context.assign(y,false);
-    context.assign(z,true);    
-    
+    context.assign(x, true);
+    context.assign(y, false);
+    context.assign(z, true);
+
     result = expression->evaluate(context);
-    
-    cout << "Overall result is " << (result == 0 ? "false": "true") << endl;
 
-	return 0;
+    cout << "Overall result is " << (result == 0 ? "false" : "true") << endl;
+
+    return 0;
 }
